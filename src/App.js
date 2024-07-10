7b1deaf70c3b966c2adcb9ee1ba02da0163b36a4
Rahul-7368 <kuarrahul.godda@gmail.com>
2024-07-10 14:11:03 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [dob, setDob] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = (e) => {
+    if(e.target.className === 'modal') {
+      setIsModalOpen(false);
+    }
+
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!username) {
+      alert('Username is required');
+      return;
+    }
+    if (!email.includes('@')) {
+      alert('Invalid email. Please check your email address.');
+      return;
+    }
+    if (phone.length !== 10) {
+      alert('Invalid phone number. Please enter a 10-digit phone number.');
+      return;
+    }
+    const today = new Date();
+    const birthDate = new Date(dob);
+    if (birthDate > today) {
+      alert('Invalid date of birth. Please enter a valid date of birth.');
+      return;
+    }
+
+    setUsername('');
+    setEmail('');
+    setDob('');
+    setPhone('');
+    setIsModalOpen(false);
+  };
+
+  return(
+    <div>
+      <h1>User Details Modal</h1>
+      <button onClick = {handleOpenModal}>Open Form</button>
+      {isModalOpen && (
+        <div className="modal" onClick={handleCloseModal}>
+          <div className="modal-content">
+            <form onSubmit={handleSubmit}>
+              <label>
+                Username:
+                <input
+                  type="text"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  />
+              </label>
+              <br />
+              <label>
+                Email:
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  />
+              </label>
+              <br />
+              <label>
+                Date of Birth:
+                <input
+                  type="date"
+                  id="dob"
+                  value={dob}
+                  onChange={(e) => setDob(e.target.value)}
+                  />
+
+              </label>
+              <br />
+              <label>
+                Phone:
+                <input
+                  type="text"
+                  id="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  />
+              </label>
+              <br/>
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+
+};
+
 
 export default App;
+
